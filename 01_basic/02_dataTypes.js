4b912f522186c473ee06b1f458841dbeb2342b73
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2024-02-15 01:54:24 +0000

    data types

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -21,3 +21,6 @@ let isLoggedIn = false
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+console.log(typeof NaN); // number
+console.log(typeof []); // object
+console.log(typeof {}); // object
