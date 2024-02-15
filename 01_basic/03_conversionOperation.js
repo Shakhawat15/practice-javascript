74877f3f86f4f0a39d94f11194f8c1a61e01b90a
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2024-02-15 02:10:38 +0000

    Conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = false
+
+
+// To Number:
+/**
+ * Number("33") => 33
+ * Number("33abc") => NaN
+ * Number(null) => 0
+ * Number(undefined) => NaN
+ * Number(true) => 1
+ * Number(false) => 0
+ */
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNunber = Number(score);
+// console.log(typeof valueInNunber);
+// console.log(valueInNunber);
+
+let isLoggedIn = 20
+// To Boolean:
+/**
+ * Boolean(1) => true
+ * Boolean(0) => false
+ * Boolean("") => false
+ * Boolean("abc") => true
+ */
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber  = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
