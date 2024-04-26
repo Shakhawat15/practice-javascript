f8a0f6cb7e89d4c596d68db967adb7e2fd3cd23a
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2024-04-26 19:27:11 +0000

    Learned about data type

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,46 @@
+"use strict" // treat all code as the newer version of JS
+
+// alert(3+3); // alert can't use in the node environment it is work in browser
+
+/**
+ * Code Readability is mendatory
+ */
+console.log(25); console.log("Shakhawat");
+console.log(3 
+    + 
+    3);  
+
+let name = "Shakhawat"
+let age = 25
+let single = true
+let city = null;
+let state;
+
+
+// There are two types of data_type in js
+
+/**
+ * Primitive:
+ * 1. int => 
+ * 2. bigint => extra large integer number
+ * 3. string => ""/''
+ * 4. boolean => true/false
+ * 5. null => standalone value
+ * 6. undefined =>
+ * 7. symbol => unique
+ */
+
+/**
+ * Non-Primitive:
+ * 1. Object
+ * 2. Array
+ */
+
+console.log(typeof "Shakhawat"); // string
+console.log(typeof 25); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // udefind
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof NaN); // number
