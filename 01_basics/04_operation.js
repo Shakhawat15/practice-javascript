5f3d34fee14474ba455900f59719a0f8c03ee4df
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2024-04-28 21:01:18 +0000

    Learn about js operations

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,19 @@
+// Operations:
+
+let str1 = "Hello"
+let str2 = " Shakhawat"
+// console.log(str1+str2); // Output => "Hello Shakhawat" 
+
+// console.log("2" + 2); // Output => 22
+// console.log(2 + "2"); // Output => 22
+// console.log(2 + "2" + 2); // Output => 222
+// console.log("2" + 2 + 2); // Output => 222
+// console.log(2 + 2 + "2"); // Output => 42
+
+// console.log(+true); // Output => 1
+// console.log(-true); // Output => -1
+// console.log(true + true); // Output => 2
+
+let gameCounter = 1
+gameCounter++
+// console.log(gameCounter); // Output => 1
