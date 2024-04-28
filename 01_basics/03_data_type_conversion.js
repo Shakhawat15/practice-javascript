b889444df45ade687ed805f1e2a77fa73f3166ef
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2024-04-28 20:33:09 +0000

    Learn Data type conversion

---
+++ 01_basics/03_data_type_conversion.js
@@ -0,0 +1,60 @@
+// Number Conversion:
+
+let score = undefined
+
+let numberConversion = Number(score)
+
+// console.log(typeof numberConversion);
+// console.log(numberConversion);
+
+/**
+ * Number Conversion:
+ * 
+ * "55" => type number value 55
+ * "" => type number value 0
+ * "Shakhawat" => type number value NaN
+ * true => type number value 1
+ * false => type number value 0
+ * "true"/"false" => type number value NaN
+ * null => type number value 0
+ * undefined => type number value NaN
+ */
+
+// Boolean Conversion:
+
+let isLoggedIn = undefined
+
+let booleanConversion = Boolean(isLoggedIn)
+
+// console.log(typeof booleanConversion);
+// console.log(booleanConversion);
+
+/**
+ * Boolean Conversion:
+ * 
+ * "true"/"false" => type boolean value true/false
+ * "" => type boolean value false
+ * "Shakhawat" type boolean value true
+ * 1/0 => type boolean value true/false
+ * null => type boolean value false
+ * undefined => type boolean value false
+ */
+
+
+// String Conversion:
+
+let stringValue = undefined
+
+let stringConversion = String(stringValue)
+
+console.log(typeof stringConversion);
+console.log(stringConversion);
+
+/**
+ * String Conversion:
+ * 
+ * 30 => type string value "30"
+ * true/false => type string value "true"/"false"
+ * null => type string value "null"
+ * undefined => type string value "undefined"
+ */
