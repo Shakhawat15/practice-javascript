17c4fcb066e5b45f954f5f516bdcd7a9e05997e3
Md. Shakhawat Hossen <53015117+Shakhawat15@users.noreply.github.com>
2024-05-03 20:57:42 +0000

    learn string

---
+++ 01_basics/07_string.js
@@ -0,0 +1,47 @@
+const name = "Shakhawat Hossen";
+
+const age = 25;
+
+// console.log("Hello my name is " + name + " I am " + age + " years old.");
+// console.log(`Hello my name is ${name} I am ${age} years old.`);
+
+const gameName = new String('shakhawat-hc');
+
+// console.log(typeof gameName);
+// console.log(typeof name);
+
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(10));
+// console.log(gameName.indexOf('c'));
+// console.log(gameName.substring(0,4)); // substring not take negative value
+// console.log(gameName.slice(-8, 5));
+// console.log(gameName.split('-'));
+
+const mood = 'Happy! '
+
+console.log(`I fell very ${mood.repeat(3)}`);
+
+const email = "  shakhawat@gmail.com       "
+
+// console.log(email);
+// console.log(email.trim());
+
+const url = "https://shakhawat.com/shakhawat%20hossen"
+
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('shakhawat'));
+// console.log(url.includes('Shakhawat'));
+
+
+// const a = "b";
+// const b = "b";
+// if (a < b) {
+//   // true
+//   console.log(`${a} is less than ${b}`);
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+
